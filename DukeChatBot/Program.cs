13fc3778fbe23378cb1b1d4c34455b6ec97e19a0
TIC4902S <gopal.issac@gmail.com>
2024-03-07 21:40:51 +0800

    string duke

--- DukeChatBot/Program.cs
+++ DukeChatBot/Program.cs
@@ -1,5 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Worlsssssd!");
-Console.WriteLine("Hello, Worlsssssd!");
-///sdasd
+string logo = " ____        _        \n"
+                    + "|  _ \\ _   _| | _____ \n"
+                    + "| | | | | | | |/ / _ \\\n"
+                    + "| |_| | |_| |   <  __/\n"
+                    + "|____/ \\__,_|_|\\_\\___|\n";
+Console.WriteLine("Hello from\n" + logo);
 

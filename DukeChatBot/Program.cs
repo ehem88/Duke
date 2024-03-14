88b01542e370b389e0c202b9670a7c9db30fa019
ehem88 <whydidudoitming@gmail.com>
2024-03-15 01:30:34 +0800

    my first commit

--- DukeChatBot/Program.cs
+++ DukeChatBot/Program.cs
@@ -1,9 +1,67 @@
 ﻿// See https://aka.ms/new-console-template for more information
-string logo = " ____        _        \n"
-                    + "|  _ \\ _   _| | _____ \n"
-                    + "| | | | | | | |/ / _ \\\n"
-                    + "| |_| | |_| |   <  __/\n"
-                    + "|____/ \\__,_|_|\\_\\___|\n";
-Console.WriteLine("Hello from\n" + logo);
-Console.WriteLine("\nHELLO ITS ME GOPAL");
+using System;
+using System.Formats.Asn1;
+using System.Reflection;
 
+namespace Boot.Y.Call
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+
+string[] items = new string[100];
+string input;
+string newItem;
+int numItems=0;
+newItem=null;
+
+string name="Boot.Y.Call";
+
+Console.WriteLine($"\n\nHello! I'm {name}"); 
+Console.WriteLine("What can I do for you?\n");
+
+Console.WriteLine("What are you feeding me today?");
+
+
+
+while(!Equals(newItem, "bye"))
+{
+  while(!Equals(newItem, "list") && !Equals(newItem, "bye"))
+  {
+    input = Console.ReadLine();
+    newItem= input;
+
+        if (!Equals(newItem, "list") && !Equals(newItem, "bye"))
+        {
+            items[numItems] = newItem;
+            numItems++;
+            Console.WriteLine("added: " + input);
+        }
+
+        else
+        {
+            if (newItem == "list")
+            {
+                for (int i = 0; i < numItems; i++)
+                {
+
+                    Console.WriteLine((i + 1) + ".\t" + items[i]);
+
+                }
+                Console.WriteLine("end list\n");
+                newItem = null;
+            }
+
+        }
+
+    }
+
+
+
+}
+Console.WriteLine("bye ass");
+
+
+    }}}
